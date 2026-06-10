c9deb8ec16404f2cfc4b0c784b7579d901cfdd47
Tutul Kumar Biswas <tutulkumar23@gmail.com>
2026-06-10 22:18:08 +0600

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Adda",
+  description: "Adda Social Media App",
+};
+ 
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
+      <body className="min-h-full flex flex-col"> 
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem 
+            disableTransitionOnChange
+          >
+        <div className="">
+          <Navbar/>
+        </div>
+          {children}
+        </ThemeProvider>
+        </body>
+    </html>
+  );
+}
